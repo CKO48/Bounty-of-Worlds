58f8d163e47fac413fddd9a0481c29ca42f439ba
unknown <kike2006.em@gmail.com>
2026-09-03 16:37:40 +0200

    Simple go discord bot
    ty Brian Morrison, nice tutorial

--- src/go/discord-bot/main.go
+++ src/go/discord-bot/main.go
@@ -1,7 +1,97 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+const prefix string = "!krug"
 
 func main() {
+	godotenv.Load("../../../.env")
+	token := os.Getenv("BOT_TOKEN")
 
+	sess, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == sess.State.User.ID {
+			return
+		}
+
+		args := strings.Split(m.Content, " ")
+
+		if args[0] != prefix {
+			return
+		}
+
+		if args[1] == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "hi!")
+		}
+
+		if args[1] == "proverbs" {
+			proverbs := []string{
+				"Don't communicate by sharing memory, share memory by communicating.",
+				"Concurrency is not parallelism.",
+				"Channels orchestrate; mutexes serialize.",
+				"The bigger the interface, the weaker the abstraction.",
+				"Make the zero value useful.",
+				"interface{} says nothing.",
+				"Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.",
+				"A little copying is better than a little dependency.",
+				"Syscall must always be guarded with build tags.",
+				"Cgo must always be guarded with build tags.",
+				"Cgo is not Go.",
+				"With the unsafe package there are no guarantees.",
+				"Clear is better than clever.",
+				"Reflection is never clear.",
+				"Errors are values.",
+				"Don't just check errors, handle them gracefully.",
+				"Design the architecture, name the components, document the details.",
+				"Documentation is for users.",
+				"Don't panic.",
+			}
+
+			selection := rand.Intn(len(proverbs))
+
+			author := discordgo.MessageEmbedAuthor{
+				Name: "Rob Pyke",
+				URL:  "https://go-proverbs.github.io",
+			}
+
+			embed := discordgo.MessageEmbed{
+				Title:  proverbs[selection],
+				Author: &author,
+			}
+
+			s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+		}
+	})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer sess.Close()
+
+	fmt.Println("Bot online")
+
+	//Waits for system notification to Close
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
